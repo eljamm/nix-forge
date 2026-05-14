021c9b42318cdc107194fe1df214324da861623b
eljamm <fedi.jamoussi@protonmail.ch>
2026-05-14 17:18:58 +0200

    wip4

--- maintainers/mk-announcement.nix
+++ maintainers/mk-announcement.nix
@@ -7,6 +7,66 @@
 lib.mapAttrs (
   name: app:
 
+  let
+    discourse = ''
+      Title: [Nix@NGI] $NAME packaged for NGI Forge
+
+      [**$NAME**]($HOMEPAGE_URL) is a $SUMMARY. This project is funded by the NGI0 $GRANT_STR grant(s).
+
+      <WHAT_CAN_PEOPLE_DO_WITH_IT>
+
+      <OTHER_COMMENTS> <THANKS_PEOPLE_INVOLVED>
+
+      <LINK_TO_TRACKING_ISSUE>
+
+      ### Try it out
+
+      Visit the [application]($APP_URL) and launch $NAME in a shell environment, container, or NixOS VM.
+
+      ### Share your feedback
+
+      Please leave your feedback using this [short survey]($SURVEY_URL).
+
+      Alternatively, join the [office hours on Jitsi]($JITSI_URL) every [Tuesday and Thursday from 15:00--16:00 CET/CEST]($CALENDAR_URL) and the [NGIpkgs Matrix channel]($MATRIX_URL) for any further comments or questions.
+
+      [Nix@NGI team webpage]($TEAM_URL).'';
+
+    nlnet = ''
+      Subject: [Nix@NGI] $NAME packaged for NGI Forge
+
+      Body:
+
+      Dear NLnet Foundation staff,
+
+      We have completed the packaging tasks for the following project:
+      - Project: $NAME
+      - Project number: <ADD_PROJECT_NUMBER>
+      - Fund: $GRANT_STR
+
+      The package is now available in the NGI Forge repository: $APP_URL.
+
+      The Nix@NGI team: $TEAM_URL.
+
+      Kind regards'';
+
+    project-author = ''
+      Subject: [Nix@NGI] $NAME packaged for NGI Forge
+
+      Body:
+
+      Dear <PROJECT_AUTHOR>,
+
+      The Nix@NGI team is an NLnet partner for packaging NGI0 funded projects. We are happy to let you know that we have packaged $NAME for the NGI Forge repository. Visit the application page at $APP_URL and launch $NAME in a shell environment, container, or NixOS VM.
+
+      Your input as the project author is very valuable for us. If you can, please leave your feedback using this short survey: $SURVEY_URL.
+
+      For more information about Nix, see: $NIX_URL.
+
+      The Nix@NGI team: $TEAM_URL.
+
+      Kind regards'';
+  in 
+
   pkgs.writeShellApplication {
     name = "announce-project";
     runtimeInputs = with pkgs; [
@@ -49,18 +109,24 @@ lib.mapAttrs (
 
       cat <<EOF
       # Discourse post
+
       \`\`\`text
-      ${lib.readFile ./discourse.md}\`\`\`
+      ${discourse}
+      \`\`\`
       ---
 
       # Email to NLnet
+
       \`\`\`text
-      ${lib.readFile ./nlnet.txt}\`\`\`
+      ${nlnet}
+      \`\`\`
       ---
 
       # Email to project author
+
       \`\`\`text
-      ${lib.readFile ./project-author.txt}\`\`\`
+      ${project-author}
+      \`\`\`
       EOF
     '';
   }
