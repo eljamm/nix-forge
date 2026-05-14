198e0fae5f826cf1da107f98bd8ce949eeaac732
eljamm <fedi.jamoussi@protonmail.ch>
2026-05-14 16:46:00 +0200

    wip2

--- maintainers/mk-announcement.nix
+++ maintainers/mk-announcement.nix
@@ -8,91 +8,113 @@ lib.mapAttrs (
   name: app:
 
   let
-    APP_URL = "https://ngi-nix.github.io/forge/app/${app.name}";
-    JITSI_URL = "https://jitsi.lassul.us/ngi-nix-office-hours";
-    CALENDAR_URL = "https://calendar.google.com/calendar/u/0/embed?src=b9o52fobqjak8oq8lfkhg3t0qg@group.calendar.google.com";
-    MATRIX_URL = "https://matrix.to/#/#ngipkgs:matrix.org";
-    TEAM_URL = "https://nixos.org/community/teams/ngi";
-    NIX_URL = "https://nix.dev";
-    SURVEY_URL = "https://nixos-foundation.notion.site/35759d49e1be81edb478e3aade9f8e95?pvs=105";
   in
 
-  pkgs.writeShellScript "mk-announce" ''
-    NAME="${app.displayName}"
-    summary=$(${pkgs.jq}/bin/jq -r '. | rtrimstr(".") as $s | ($s[0:1] | ascii_downcase) + $s[1:]' <<<\"${app.description}\")
-    homepage_url="foobar"
-    grant_str="foobar"
+  pkgs.writeShellApplication {
+    name = "announce-${app.displayName}";
+    runtimeInputs = with pkgs; [
+      jq
+    ];
+    text = ''
+      set -eux
 
-    cat <<EOF
-    # Discourse post
-    \`\`\`text
-    Title: [Nix@NGI] $NAME packaged for NGI Forge
+      APP_URL="https://ngi-nix.github.io/forge/app/${app.name}"
+      JITSI_URL="https://jitsi.lassul.us/ngi-nix-office-hours"
+      CALENDAR_URL="https://calendar.google.com/calendar/u/0/embed?src=b9o52fobqjak8oq8lfkhg3t0qg@group.calendar.google.com"
+      MATRIX_URL="https://matrix.to/#/#ngipkgs:matrix.org"
+      TEAM_URL="https://nixos.org/community/teams/ngi"
+      NIX_URL="https://nix.dev"
+      SURVEY_URL="https://nixos-foundation.notion.site/35759d49e1be81edb478e3aade9f8e95?pvs=105"
+      NAME="${app.displayName}"
+      SUMMARY=$(echo "\"${app.description}\"" | \
+        jq -r '
+          . |
+          rtrimstr(".") as $s |
+          ($s[0:1] | ascii_downcase) + $s[1:]
+        ')
+      HOMEPAGE_URL=$(echo "\"${builtins.toJSON app.links}\"" | \
+        jq -r '
+          if .website != null then .website.url
+          elif .source != null then .source.url
+          else "<ADD_HOMEPAGE_URL>"
+          end
+      ')
+      GRANT_STR=$(echo "\"${builtins.toJSON app.ngi}\"" | \
+        jq -r '
+          [ .grants | to_entries[] | select(.value | length > 0) | .key ] | join(", ")
+      ')
 
-    [**$NAME**]($homepage_url) is a $summary. This project is funded by the NGI0 $grant_str grant(s).
+      cat <<EOF
+      # Discourse post
+      \`\`\`text
+      Title: [Nix@NGI] $NAME packaged for NGI Forge
 
-    <WHAT_CAN_PEOPLE_DO_WITH_IT>
+      [**$NAME**]($HOMEPAGE_URL) is a $SUMMARY. This project is funded by the NGI0 $GRANT_STR grant(s).
 
-    <OTHER_COMMENTS> <THANKS_PEOPLE_INVOLVED>
+      <WHAT_CAN_PEOPLE_DO_WITH_IT>
 
-    <LINK_TO_TRACKING_ISSUE>
+      <OTHER_COMMENTS> <THANKS_PEOPLE_INVOLVED>
 
-    ### Try it out
+      <LINK_TO_TRACKING_ISSUE>
 
-    Visit the [application](${APP_URL}) and launch $NAME in a shell environment, container, or NixOS VM.
+      ### Try it out
 
-    ### Share your feedback
+      Visit the [application]($APP_URL) and launch $NAME in a shell environment, container, or NixOS VM.
 
-    Please leave your feedback using this [short survey](${SURVEY_URL}).
+      ### Share your feedback
 
-    Alternatively, join the [office hours on Jitsi](${JITSI_URL}) every [Tuesday and Thursday from 15:00--16:00 CET/CEST](${CALENDAR_URL}) and the [NGIpkgs Matrix channel](${MATRIX_URL}) for any further comments or questions.
+      Please leave your feedback using this [short survey]($SURVEY_URL).
 
-    [Nix@NGI team webpage](${TEAM_URL}).
-    \`\`\`
+      Alternatively, join the [office hours on Jitsi]($JITSI_URL) every [Tuesday and Thursday from 15:00--16:00 CET/CEST]($CALENDAR_URL) and the [NGIpkgs Matrix channel]($MATRIX_URL) for any further comments or questions.
 
-    ---
+      [Nix@NGI team webpage]($TEAM_URL).
+      \`\`\`
 
-    # Email to NLnet
+      ---
 
-    \`\`\`text
-    Subject: [Nix@NGI] $NAME packaged for NGI Forge
+      # Email to NLnet
 
-    Body:
+      \`\`\`text
+      Subject: [Nix@NGI] $NAME packaged for NGI Forge
 
-    Dear NLnet Foundation staff,
+      Body:
 
-    We have completed the packaging tasks for the following project:
-    - Project: $NAME
-    - Project number: <ADD_PROJECT_NUMBER>
-    - Fund: $grant_str
+      Dear NLnet Foundation staff,
 
-    The package is now available in the NGI Forge repository: ${APP_URL}.
+      We have completed the packaging tasks for the following project:
+      - Project: $NAME
+      - Project number: <ADD_PROJECT_NUMBER>
+      - Fund: $GRANT_STR
 
-    The Nix@NGI team: ${TEAM_URL}.
+      The package is now available in the NGI Forge repository: $APP_URL.
 
-    Kind regards
-    \`\`\`
+      The Nix@NGI team: $TEAM_URL.
 
-    ---
+      Kind regards
+      \`\`\`
 
-    # Email to project author
+      ---
 
-    \`\`\`text
-    Subject: [Nix@NGI] $NAME packaged for NGI Forge
+      # Email to project author
 
-    Body:
+      \`\`\`text
+      Subject: [Nix@NGI] $NAME packaged for NGI Forge
 
-    Dear <PROJECT_AUTHOR>,
+      Body:
 
-    The Nix@NGI team is an NLnet partner for packaging NGI0 funded projects. We are happy to let you know that we have packaged $NAME for the NGI Forge repository. Visit the application page at ${APP_URL} and launch $NAME in a shell environment, container, or NixOS VM.
+      Dear <PROJECT_AUTHOR>,
 
-    Your input as the project author is very valuable for us. If you can, please leave your feedback using this short survey: ${SURVEY_URL}.
+      The Nix@NGI team is an NLnet partner for packaging NGI0 funded projects. We are happy to let you know that we have packaged $NAME for the NGI Forge repository. Visit the application page at $APP_URL and launch $NAME in a shell environment, container, or NixOS VM.
 
-    For more information about Nix, see: ${NIX_URL}.
+      Your input as the project author is very valuable for us. If you can, please leave your feedback using this short survey: $SURVEY_URL.
 
-    The Nix@NGI team: ${TEAM_URL}.
+      For more information about Nix, see: $NIX_URL.
 
-    Kind regards
-    \`\`\`
-    EOF
-  ''
+      The Nix@NGI team: $TEAM_URL.
+
+      Kind regards
+      \`\`\`
+      EOF
+    '';
+  }
 ) apps
